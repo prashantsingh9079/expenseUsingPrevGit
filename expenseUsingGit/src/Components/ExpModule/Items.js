a795a0d0ac52035f4e5ee49df8dc03830db15639
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-06-06 15:26:11 +0530

    added a temp delete button using normal dom

--- expenseUsingGit/src/Components/ExpModule/Items.js
+++ expenseUsingGit/src/Components/ExpModule/Items.js
@@ -3,6 +3,14 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
  const Items=(props)=>
 {   
+    const deletHandler = (e) =>{
+        const buttonClickedParent = e.target.parentNode.parentNode;
+        const buttonDate = buttonClickedParent.firstChild;
+        buttonClickedParent.removeChild(buttonDate)
+        buttonClickedParent.removeChild(e.target.parentNode);
+        console.log(buttonClickedParent)
+         
+    }
     
     return (
         <div className="expense-item">
@@ -12,6 +20,7 @@ import ExpenseDetails from './ExpenseDetails';
                 <h2>{props.location}</h2>
                 <div className="expense-item__price"> Rs {props.price}</div>
                 <ExpenseDetails location={props.location} price={props.price} desc={props.desc}/>
+                <button onClick={deletHandler}>Delete</button>
             </div>
             
         </div>
