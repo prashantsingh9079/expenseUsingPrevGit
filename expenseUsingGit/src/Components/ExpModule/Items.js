c6b3caec776a15ea5abe79d9286900329c79d2b8
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-06-06 16:52:04 +0530

    added onr button to over write price to 100 in dom

--- expenseUsingGit/src/Components/ExpModule/Items.js
+++ expenseUsingGit/src/Components/ExpModule/Items.js
@@ -1,26 +1,30 @@
 import './Item.css'
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
+import React,{useState} from 'react'
  const Items=(props)=>
 {   
-    const deletHandler = (e) =>{
-        const buttonClickedParent = e.target.parentNode.parentNode;
-        const buttonDate = buttonClickedParent.firstChild;
-        buttonClickedParent.removeChild(buttonDate)
-        buttonClickedParent.removeChild(e.target.parentNode);
-        console.log(buttonClickedParent)
-         
+    const [amount,setAmount] = useState(props.price)
+    const [title, setTitle] = useState(props.desc)
+    const buttonHandler = () =>{
+        setTitle("updated!!!")
+    }
+
+    const amountHandler = (e)=>{
+        setAmount(100)
+        console.log(amount)
     }
     
     return (
         <div className="expense-item">
            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.desc}</h2>
+                <h2>{title}</h2>
                 <h2>{props.location}</h2>
-                <div className="expense-item__price"> Rs {props.price}</div>
-                <ExpenseDetails location={props.location} price={props.price} desc={props.desc}/>
-                <button onClick={deletHandler}>Delete</button>
+                <div className="expense-item__price"> Rs {amount}</div>
+                <ExpenseDetails location={props.location} price={amount} desc={title}/>
+                <button onClick={buttonHandler}>Title Changed</button>
+                <button onClick={amountHandler}>Change amount</button>
             </div>
             
         </div>
